742d808c88ffa227ccb4ce584eaf33a5aaa86f54
lucas.fernandez <85810432+Lucasfernan152@users.noreply.github.com>
2023-06-09 12:11:57 -0300

    creates android app

---
+++ app/src/main/java/com/example/operacionesymantenimiento/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.operacionesymantenimiento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
