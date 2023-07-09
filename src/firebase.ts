12e000a565b01b22bf9f25e96034bc6ca47ab59c
lucas.fernandez <85810432+Lucasfernan152@users.noreply.github.com>
2023-07-09 19:17:22 -0300

    change to react

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIcNxaDTcLyg1pyPqRu1xAxtDnkI6vYnE",
+  authDomain: "operaciones-y-mantenimie-71ced.firebaseapp.com",
+  projectId: "operaciones-y-mantenimie-71ced",
+  storageBucket: "operaciones-y-mantenimie-71ced.appspot.com",
+  messagingSenderId: "1093766863963",
+  appId: "1:1093766863963:web:14bafc3dbe761cef64f37e"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
